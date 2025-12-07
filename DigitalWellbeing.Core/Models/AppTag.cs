0cea079450016383da336ba1ca1f862288e9bb35
Some1sm <ceperodavid@gmail.com>
2025-12-07 15:22:56 +0100

    Move AppTag enum to Core library

---
+++ DigitalWellbeing.Core/Models/AppTag.cs
@@ -0,0 +1,13 @@
+namespace DigitalWellbeing.Core.Models
+{
+    public enum AppTag
+    {
+        Untagged = 0,
+        Work = 1,
+        Education = 2,
+        Entertainment = 3,
+        Social = 4,
+        Utility = 5,
+        Game = 6
+    }
+}
